4a3d6776409f8c4e2405f2018ec6b7623b9ce901
Anders Eriksson <anderse@unseen.is>
2018-09-08 17:16:29 +0200

    created login+register pages+css

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP - Labb 2 - Anders Eriksson</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <form action="" method="post">
+            <fieldset>
+                <legend><strong>USER LOGIN</strong></legend>
+                    <label for="username">User name</label>
+                    <input class="text-input" name="username" type="text">
+                    <label for="password">Password</label>
+                    <input class="text-input" name="password" type="password">
+            </fieldset>
+            <input class="btn" type="submit" name="submit" value="LOGIN">
+            <p><small>Don't have an account? Please <a href="register.php">register here</a></small></p>
+        </form>
+        <a href=""></a>
+    </div>
+    
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create account</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <form action="" method="post">
+            <fieldset>
+                <legend><strong>CREATE YOUR ACCOUNT</strong></legend>
+                    <label for="username">User name</label>
+                    <input class="text-input" name="username" type="text">
+                    <label for="password">Password</label>
+                    <input class="text-input" name="password" type="password">
+            </fieldset>
+            <input class="btn" type="submit" name="submit" value="CREATE ACCOUNT">
+        </form>
+    </div>
+    
+</body>
+</html>
