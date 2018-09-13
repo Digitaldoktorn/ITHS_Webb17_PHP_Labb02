cc65f580df2f0b78df7d4e0104a743d37dabf56c
Anders Eriksson <anderse@unseen.is>
2018-09-13 20:02:48 +0200

    added "salt" but not complete

--- class.User.php
+++ class.User.php
@@ -28,16 +28,21 @@
             }
           }
 
-        // Register user - saving username and password into csv file
+        // Registering user - saving username and password into csv file
         public function saveUserName() {
-            $fileHandle = fopen($this->userName . ".csv", "w+"); // lagt in $this->userName
-            // $writeString = serialize($this->userName) . serialize($this->userPassword) . PHP_EOL;
-            $writeString = ($this->userName) . ',' . ($this->userPassword) . PHP_EOL;
+            $thisUserName = $this->userName;
+            $thisUserPassword = $this->userPassword;
+
+            // hashing password
+            $hashedPassword = password_hash($thisUserPassword, PASSWORD_DEFAULT);
+
+            $fileHandle = fopen($thisUserName . ".csv", "w+");
+            $writeString = ($thisUserName) . ',' . ($hashedPassword) . PHP_EOL;
             fwrite($fileHandle, $writeString);
             fclose($fileHandle);
         }
 
-        // create form function
+        // Creating form
         public function getForm($page) {
             $formString = "<form action=$page method='POST'>";
         
@@ -57,11 +62,14 @@
             
             $csvUserData = array_map('str_getcsv', file($postData['userName'] . ".csv"));
             $csvUserName = $csvUserData[0][0];
-            $csvUserPassword = $csvUserData[0][1];
+            $csvHashedPassword = $csvUserData[0][1];
+            $userInputPassword = $postData['userPassword'];
+            $verifiedPassword = password_verify($userInputPassword, $csvHashedPassword);
 
             // print_r($csvUserData);
             // print_r($csvUserName);
-            // print_r($csvUserPassword);
+            // print_r($csvHashedPassword);
+            print_r($verifiedPassword);
 
             if($postData['userName'] != $csvUserName || $postData['userPassword'] != $csvUserPassword){
                 echo "<p id='alert'>Your username and/or password was not entered correctly. Please try again.</p>";?>
@@ -72,7 +80,7 @@
                 </script><?php
             }
 
-            elseif($postData['userName'] = $csvUserName || $postData['userPassword'] = $csvUserPassword){
+            elseif($postData['userName'] == $csvUserName || $verifiedPassword == 1){
                 header("Location: profile_page.php");
             }
 
